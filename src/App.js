8e7d15d815b4f4ef7f324b0832b27f83b42d32cb
Paweł Kownacki <koowal@gmail.com>
2020-06-07 22:03:47 +0100

    carousel

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { Router, Link } from '@reach/router';
 import Details from './Details';
 import SearchParams from './SearchParams';
+import ThemeContext from './ThemeContext';
 
 const App = () => {
+  const themeHook = useState('orange');
+  console.log(themeHook);
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
