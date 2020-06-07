8bb24a8e4ab247ebcb402479bb911e94b17f206d
Paweł Kownacki <koowal@gmail.com>
2020-06-07 21:05:59 +0100

    fix+error

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import pet from '@frontendmasters/pet';
+import Carousel from './Carousel';
+import ErrorBoundary from './ErrorBoundary';
 
 class Details extends React.Component {
-  state = { loading: true };
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
@@ -21,10 +28,11 @@ class Details extends React.Component {
     if (this.state.loading) {
       return <h1>loading ...</h1>;
     }
-    const { animal, breed, location, description, name } = this.state;
+    const { animal, breed, location, description, name, media } = this.state;
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
@@ -36,4 +44,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsWithErorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
